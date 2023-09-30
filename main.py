58f1ab15624c4d19085779a66f51ebe4ede5cbbe
HaroldOlaya <haroldolaya.2499@hotmail.com>
2023-09-30 00:56:11 -0500

    hora,fecha y clima

--- main.py
+++ main.py
@@ -1,23 +1,95 @@
+import requests
+from datetime import datetime
 import speech_recognition as sr
 import pyttsx3
 import socket
 
-# Función para enviar datos al ESP32
+def clima():
+    # Definir la URL de la API y la clave de API
+    url = 'https://api.tomorrow.io/v4/timelines'
+    api_key = 'bSZp300M1YBDkZoVryBmlBo8Sq6gmZxI'
+
+    # Latitud y longitud de Bogotá, Colombia
+    latitud = '4.6097'
+    longitud = '-74.0817'
+
+    # Definir los parámetros de la solicitud
+    params = {
+        'location': f'{latitud},{longitud}',  # Latitud y longitud de Bogotá
+        'fields': 'temperature',  # Solicitar solo la temperatura
+        'timesteps': '1h',  # Paso de tiempo de 1 hora
+        'units': 'metric',  # Unidades métricas (grados Celsius)
+        'apikey': api_key
+    }
+
+    # Realizar la solicitud GET a la API
+    respuesta = requests.get(url, params=params)
+
+    # Comprobar si la solicitud fue exitosa (código de estado 200)
+    if respuesta.status_code == 200:
+        datos_clima = respuesta.json()
+
+        # Extraer la temperatura actual
+        temperatura_actual = datos_clima['data']['timelines'][0]['intervals'][0]['values']['temperature']
+
+        # Imprimir la temperatura actual
+        climaBogota=(f'Temperatura actual en Bogotá: {temperatura_actual}°C')
+        return climaBogota
+    else:
+        return ('Error al obtener la temperatura actual del clima en Bogotá')
+def tiempo():
+    pregunta = "Necesitas hora o fecha"
+    # respuesta en voz alta
+    text_to_speech.say(pregunta)
+    text_to_speech.runAndWait()
+    with sr.Microphone() as source:
+        print("Hora o Fecha")
+        audio = recognizer.listen(source)
+    # Reconoce el discurso utilizando el motor de reconocimiento de voz
+    text = recognizer.recognize_google(audio, language="es-ES")
+    print(f"Texto reconocido: {text}")
+    # URL para obtener la hora actual en Bogotá
+    url = 'https://worldtimeapi.org/api/timezone/America/Bogota'
+
+    # Realizar la solicitud GET a la API
+    respuesta = requests.get(url)
+
+    # Comprobar si la solicitud fue exitosa (código de estado 200)
+    if respuesta.status_code == 200:
+        datos_hora = respuesta.json()
+
+        # Extraer y mostrar la hora actual
+        hora_actual = datos_hora['datetime']
+        # Cadena de fecha y hora
+        cadena_fecha_hora = hora_actual
+
+        # Convertir la cadena a un objeto datetime
+        fecha_hora_objeto = datetime.fromisoformat(cadena_fecha_hora)
+
+        # Extraer la fecha y la hora por separado
+        fecha = fecha_hora_objeto.date()
+        hora = fecha_hora_objeto.time()
+        if text=="hora" or text == "Hora":
+            hora_str = str(hora)
+            partes_hora = hora_str.split(':')
+            return (f'Son las: {partes_hora[0]},con {partes_hora[1]} minutos y {partes_hora[2][:2]} segundos')
+        elif text=="fecha" or text =="Fecha":
+            return(f'Fecha: {fecha}')
+    else:
+        return('Error al obtener la hora actual en Bogotá')
 def enviar(valor):
     client_socket.send((valor).encode())
 
 esp32_ip = "192.168.1.44"
 esp32_port = 80
-
-# Crear un socket TCP
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Conectar al ESP32
 client_socket.connect((esp32_ip, esp32_port))
-# Inicializar el reconocimiento de voz y el motor de texto a voz
+recognizer = sr.Recognizer()
+text_to_speech = pyttsx3.init()
 recognizer = sr.Recognizer()
 text_to_speech = pyttsx3.init()
 iniciar=True
+
 while iniciar:
     try:
         # Captura audio del micrófono
@@ -32,20 +104,27 @@ while iniciar:
             #respuesta en voz alta
             text_to_speech.say(respuesta)
             text_to_speech.runAndWait()
-
-
             while True:
                 with sr.Microphone() as source:
-                    print("Cuenteme...")
+                    print("cuenteme...")
                     audio = recognizer.listen(source)
+                    # Reconoce el discurso utilizando el motor de reconocimiento de voz
                     text = recognizer.recognize_google(audio, language="es-ES")
                     print(f"Texto reconocido: {text}")
-                    if text=="Salir" or text == "salir":
-                        despedir="Fue un placer señor STARk"
-                        text_to_speech.say(despedir)
+                    if text == "salir":
+                        respuesta = "Fue un placer señor stark"
+                        # respuesta en voz alta
+                        text_to_speech.say(respuesta)
                         text_to_speech.runAndWait()
-                        iniciar=False
                         break
+                    elif text == "clima" or text=="Clima":
+                        climaBogota=clima()
+                        text_to_speech.say(climaBogota)
+                        text_to_speech.runAndWait()
+                    elif text== "Tiempo" or text=="tiempo":
+                        tiempoActual = tiempo()
+                        text_to_speech.say(tiempoActual)
+                        text_to_speech.runAndWait()
                     elif text=="Encender luz":
                         despedir = "encendiendo luz"
                         text_to_speech.say(despedir)
@@ -60,17 +139,8 @@ while iniciar:
                         for i in range(10):
                             enviar("b")
                         print("Apagando luz")
-
     except sr.UnknownValueError:
         print("No se pudo entender el audio.")
     except sr.RequestError as e:
         print(f"Error en la solicitud: {e}")
-
-
-
-
-
-
-
-
 
